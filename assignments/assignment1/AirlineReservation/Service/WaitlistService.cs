e07071f9fe46b4cd3d20f9f898189543ef3de459
GusRodrigues <omykronbr@gmail.com>
2020-01-20 22:20:23 -0500

    Waitlist Service encapsulates waitlist

---
+++ assignments/assignment1/AirlineReservation/Service/WaitlistService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirlineReservation
+{
+    /// <summary>
+    /// Provides operations of the useCases of Waitlist.
+    /// </summary>
+    public class WaitlistService
+    {
+        /// <summary>
+        /// The waitlist to operate
+        /// </summary>
+        private Waitlist waitlist;
+
+        public WaitlistService()
+        {
+            this.waitlist = new Waitlist();
+        }
+
+        /// <summary>
+        /// Copy of the array holding all customers waitlisted
+        /// </summary>
+        /// <returns></returns>
+        public string[] Waitlist() => waitlist.WaitlistedCustomers();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        public bool AddToWaitlist(string customer)
+        {
+            // String validation to prevent null/whitespaced
+            if (String.IsNullOrWhiteSpace(customer))
+            {
+                throw new NullReferenceException("Customer must have a name");
+            }
+            return waitlist.Enqueue(customer);
+        }
+
+        /// <summary>
+        /// Attempts to remove customer from the waitlist.
+        /// 
+        /// Returns the customer, if any. Otherwise, returns an
+        /// empty string.
+        /// </summary>
+        /// <returns></returns>
+        public string RemoveFromTheWaitlist() => waitlist.Dequeue();
+    }
+}
