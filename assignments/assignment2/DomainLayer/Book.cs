cb5650ce8f149800cddecf82142c5336843fc32e
GusRodrigues <omykronbr@gmail.com>
2020-02-06 00:01:12 -0500

    Configuring base structure

---
+++ assignments/assignment2/DomainLayer/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("BookManagementAssignment2Tests")]
+namespace DomainLayer
+{
+    public class Book
+    {
+        private string Title;
+        private int ISBN;
+        private string Author;
+        private DateTime PublishedDate;
+        private string MailingAddress;
+        private string StreetOrRoute;
+        private string Town;
+        private string Country;
+        private string Province;
+        private string PostalCode;
+        private string HomePhone;
+        private string CellPhone;
+        private string Email;
+
+    }
+}
