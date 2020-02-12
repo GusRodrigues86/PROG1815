3691f19c86042a575bb4f9ec99208561154f1453
GusRodrigues <omykronbr@gmail.com>
2020-02-12 16:24:09 -0500

    Só vai

---
+++ exercises/inClass/alarm/AlarmClock/View/AlarmClock.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AlarmClock
+{
+    partial class GBRAlarmClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblErrors = new System.Windows.Forms.Label();
+            this.btnStopAlarm = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.checkActive = new System.Windows.Forms.CheckBox();
+            this.comboAmPm = new System.Windows.Forms.ComboBox();
+            this.txtAlarmTimer = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblTime.Location = new System.Drawing.Point(11, 24);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(190, 44);
+            this.lblTime.TabIndex = 0;
+            this.lblTime.Text = "00:00 AM";
+            // 
+            // lblErrors
+            // 
+            this.lblErrors.AutoSize = true;
+            this.lblErrors.ForeColor = System.Drawing.Color.Red;
+            this.lblErrors.Location = new System.Drawing.Point(14, 135);
+            this.lblErrors.Margin = new System.Windows.Forms.Padding(6);
+            this.lblErrors.Name = "lblErrors";
+            this.lblErrors.Size = new System.Drawing.Size(33, 13);
+            this.lblErrors.TabIndex = 1;
+            this.lblErrors.Text = "errors";
+            // 
+            // btnStopAlarm
+            // 
+            this.btnStopAlarm.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnStopAlarm.FlatAppearance.BorderSize = 0;
+            this.btnStopAlarm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStopAlarm.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnStopAlarm.Location = new System.Drawing.Point(226, 27);
+            this.btnStopAlarm.Margin = new System.Windows.Forms.Padding(6);
+            this.btnStopAlarm.Name = "btnStopAlarm";
+            this.btnStopAlarm.Size = new System.Drawing.Size(75, 44);
+            this.btnStopAlarm.TabIndex = 4;
+            this.btnStopAlarm.Text = "STOP ALARM";
+            this.btnStopAlarm.UseVisualStyleBackColor = false;
+            this.btnStopAlarm.Click += new System.EventHandler(this.StopAlarm);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnAbout.FlatAppearance.BorderSize = 0;
+            this.btnAbout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAbout.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnAbout.Location = new System.Drawing.Point(226, 83);
+            this.btnAbout.Margin = new System.Windows.Forms.Padding(6);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 5;
+            this.btnAbout.Text = "about";
+            this.btnAbout.UseVisualStyleBackColor = false;
+            this.btnAbout.Click += new System.EventHandler(this.btnAbout_Click);
+            // 
+            // checkActive
+            // 
+            this.checkActive.AutoSize = true;
+            this.checkActive.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.checkActive.Location = new System.Drawing.Point(14, 106);
+            this.checkActive.Margin = new System.Windows.Forms.Padding(6);
+            this.checkActive.Name = "checkActive";
+            this.checkActive.Size = new System.Drawing.Size(85, 17);
+            this.checkActive.TabIndex = 3;
+            this.checkActive.Text = "Alarm Active";
+            this.checkActive.UseVisualStyleBackColor = true;
+            this.checkActive.Click += new System.EventHandler(this.ChangeAlarmState);
+            // 
+            // comboAmPm
+            // 
+            this.comboAmPm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboAmPm.FormattingEnabled = true;
+            this.comboAmPm.Items.AddRange(new object[] {
+            "AM",
+            "PM"});
+            this.comboAmPm.Location = new System.Drawing.Point(131, 73);
+            this.comboAmPm.Margin = new System.Windows.Forms.Padding(6);
+            this.comboAmPm.MaxDropDownItems = 2;
+            this.comboAmPm.Name = "comboAmPm";
+            this.comboAmPm.Size = new System.Drawing.Size(60, 21);
+            this.comboAmPm.TabIndex = 2;
+            this.comboAmPm.Leave += new System.EventHandler(this.ValidateTime);
+            // 
+            // txtAlarmTimer
+            // 
+            this.txtAlarmTimer.Location = new System.Drawing.Point(14, 74);
+            this.txtAlarmTimer.Margin = new System.Windows.Forms.Padding(6);
+            this.txtAlarmTimer.Mask = "90:00";
+            this.txtAlarmTimer.Name = "txtAlarmTimer";
+            this.txtAlarmTimer.Size = new System.Drawing.Size(100, 20);
+            this.txtAlarmTimer.TabIndex = 1;
+            this.txtAlarmTimer.ValidatingType = typeof(System.DateTime);
+            this.txtAlarmTimer.Leave += new System.EventHandler(this.ValidateTime);
+            // 
+            // GBRAlarmClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(52)))), ((int)(((byte)(52)))));
+            this.ClientSize = new System.Drawing.Size(312, 184);
+            this.Controls.Add(this.txtAlarmTimer);
+            this.Controls.Add(this.comboAmPm);
+            this.Controls.Add(this.checkActive);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnStopAlarm);
+            this.Controls.Add(this.lblErrors);
+            this.Controls.Add(this.lblTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "GBRAlarmClock";
+            this.Padding = new System.Windows.Forms.Padding(8, 24, 8, 12);
+            this.Text = "GBR Alarm Clock";
+            this.Load += new System.EventHandler(this.GBRAlarmClock_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblErrors;
+        private System.Windows.Forms.Button btnStopAlarm;
+        private System.Windows.Forms.Button btnAbout;
+        private System.Windows.Forms.CheckBox checkActive;
+        private System.Windows.Forms.ComboBox comboAmPm;
+        private System.Windows.Forms.MaskedTextBox txtAlarmTimer;
+    }
+}
+
---
+++ exercises/inClass/alarm/AlarmClock/View/AlarmClock.cs
@@ -0,0 +1,201 @@
+﻿/* inClass 2
+ * 
+ * Revision History
+ *      Gustavo Bonifacio Rodrigues, 2020.02.12: Created
+ */
+using System;
+using System.Globalization;
+using System.Media;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace AlarmClock
+{
+    public partial class GBRAlarmClock : Form
+    {
+        /// <summary>
+        /// Defines if the alarm is armed
+        /// </summary>
+        private bool IsArmed;
+        /// <summary>
+        /// The Alarm time
+        /// </summary>
+        private DateTime AlarmTime;
+        /// <summary>
+        /// Ticker
+        /// </summary>
+        private Timer Timer;
+        /// <summary>
+        /// sound player
+        /// </summary>
+        private SoundPlayer Player;
+        /// <summary>
+        /// Is alarm ringing
+        /// </summary>
+        private bool IsRinging;
+
+        public GBRAlarmClock()
+        {
+            InitializeComponent();
+            lblErrors.Text = "";
+            IsArmed = false;
+            Timer = new Timer();
+            Player = new SoundPlayer();
+            IsRinging = false;
+        }
+
+        /// <summary>
+        /// Load operation. Will start the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void GBRAlarmClock_Load(object sender, EventArgs e)
+        {
+            Timer.Interval = 1000; //
+            Timer.Tick += new EventHandler(this.Timer_Tick);
+            Timer.Start();
+
+        }
+
+        /// <summary>
+        /// Every tick of the timer, update the time label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            lblTime.Text = DateTime.Now.ToString("hh:mm tt", CultureInfo.InvariantCulture);
+
+            TimeSpan tolerance = new TimeSpan(0, 0, 4);
+            var pastAlarm = ((DateTime.Now - AlarmTime) >= tolerance);
+            var futureAlarm = ((AlarmTime - DateTime.Now) >= tolerance);
+
+            if (IsArmed)
+            {
+                if (!futureAlarm && !pastAlarm)
+                {
+                    IsRinging = true;
+                    PlayAlarm(sender, e);
+                }
+            }
+            if (IsRinging)
+            {
+                PlayAlarm(sender, e);
+            }
+        }
+
+        #region Validation
+
+        /// <summary>
+        /// Verify if the user provided input is valid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ValidateTime(object sender, EventArgs e)
+        {
+            Regex timeRegex = new Regex(@"^[0-1][0-9][:][0-5][0-9]",
+                RegexOptions.IgnoreCase);
+            string timeInput = txtAlarmTimer.Text;
+            // clear errors
+            lblErrors.Text = "";
+            if (timeInput.Equals("  :  ")) // empty time format by masked textbox
+            {
+                lblErrors.Text += "The format is in 12 hour HH:MM\r\n";
+                IsArmed = false;
+                return;
+            }
+            else
+            {
+                timeInput = timeInput.Trim();
+                if (!timeRegex.IsMatch(timeInput))
+                {
+                    IsArmed = false;
+                    lblErrors.Text += "The format is in 12 hour HH:MM\r\n";
+                }
+            }
+
+            // validate combobox for am/pm
+            if (comboAmPm.SelectedItem is null)
+            {
+                lblErrors.Text += "AM or PM?";
+            }
+        }
+        #endregion
+
+        #region Change Alarm State
+        /// <summary>
+        /// Changes the Alarm state. It only arms if the information provided is valid
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ChangeAlarmState(object sender, EventArgs e)
+        {
+            // disable the alarm
+            if (IsArmed)
+            {
+                SilenceAlarm();
+                IsArmed = false;
+                checkActive.Checked = false;
+            }
+
+            // Sanity validation
+            ValidateTime(sender, e);
+
+            // if is not ok, stop and disarm
+            if (!String.IsNullOrWhiteSpace(lblErrors.Text))
+            {
+                checkActive.Checked = false;
+                IsArmed = false;
+                return;
+            }
+
+            // Create a date time based on the user input
+            AlarmTime = DateTime.ParseExact($"{txtAlarmTimer.Text} " +
+                $"{comboAmPm.SelectedItem.ToString()}",
+                "hh:mm tt", // used format
+                CultureInfo.InvariantCulture);
+
+            // if AlarmTime >= Current Time + 3s, arm.
+            if (AlarmTime.CompareTo(DateTime.Now.AddSeconds(3)) >= 0)
+            {
+                IsArmed = true;
+            }
+
+        }
+        #endregion
+
+        private void PlayAlarm(object sender, EventArgs e)
+        {
+            Player.SoundLocation = @"c:\windows\media\Windows Logoff Sound.wav";
+            Player.Play();
+        }
+
+        /// <summary>
+        /// Quiets the alarm ftfs.
+        /// </summary>
+        private void SilenceAlarm() =>
+            Player.Stop();
+
+        /// <summary>
+        /// Disable the alarm and force the player to stop if playing
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StopAlarm(object sender, EventArgs e)
+        {
+            SilenceAlarm();
+            IsRinging = false;
+        }
+
+        /// <summary>
+        /// MessageBox containing about me
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAbout_Click(object sender, EventArgs e)
+        {
+            string author = "Written and designed by Gustavo Bonifacio Rodrigues\r\n" +
+            MessageBox.Show(author, "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
