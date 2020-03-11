f2f1960788953e9ed4dc3df7e983eb3c71894ab1
GusRodrigues <omykronbr@gmail.com>
2020-03-11 12:36:33 -0400

    Utility class for file readers and test cases

---
+++ assignments/assignment3/PurchaseOrder.Tests/Util/FileUtilsTest.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using PurchaseOrder.Util;
+
+namespace PurchaseOrder.Tests.Util
+{
+    class FileUtilsTest
+    {
+        private string pathToProject;
+        #region Before and After
+        [SetUp]
+        public void Init()
+        {
+            pathToProject = Path.GetDirectoryName(System.AppDomain.CurrentDomain.BaseDirectory);
+            pathToProject = Directory.GetParent(pathToProject).FullName;
+            pathToProject = Directory.GetParent(pathToProject).FullName;
+            pathToProject = Directory.GetParent(pathToProject).FullName;
+            // checks if there is any test file saved.
+            if (File.Exists(pathToProject + @"/Util/TestText.txt"))
+            {
+                File.Delete(pathToProject + @"/Util/TestText.txt");
+            }
+        }
+
+        [TearDown]
+        public void Dispose()
+        {
+            if (File.Exists(pathToProject + @"/Util/TestText.txt"))
+            {
+                File.Delete(pathToProject + @"/Util/TestText.txt");
+            }
+        }
+        #endregion
+
+        [Test]
+        public void CantFindFileThrowsException()
+        {
+            Assert.Throws<FileNotFoundException>(
+                () =>
+                {
+                    FileUtils.CSVReader("file");
+                });
+            Assert.Throws<ArgumentNullException>(
+                () =>
+                {
+                    FileUtils.CSVReader(null);
+                });
+            Assert.Throws<DirectoryNotFoundException>(
+                () =>
+                {
+                    FileUtils.CSVReader(@"Z:\doesnt\exist.file");
+                });
+        }
+        [Test]
+        public void CanReadAnExistingCSVFile()
+        {
+            try
+            {
+                using (StreamReader file = FileUtils.CSVReader(pathToProject + @"./Util/hello.csv"))
+                {
+                    string splited = file.ReadLine();
+                    Assert.AreEqual("Hello,World", splited);
+                }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+        [Test]
+        public void CanCreatFileIfDoesntExist()
+        {
+            string fileName = pathToProject + @"/Util/TestText.txt";
+            Assert.IsTrue(FileUtils.CreateFile(fileName));
+        }
+        [Test]
+        public void IfFileAlreadyExistisReturnsFalse()
+        {
+            string fileName = pathToProject + @"/Util/hello.csv";
+            Assert.IsFalse(FileUtils.CreateFile(fileName));
+        }
+    }
+}
---
+++ assignments/assignment3/PurchaseOrder.Util/FileUtils.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PurchaseOrder.Util
+{
+    /// <summary>
+    /// Holds readers for the project.
+    /// </summary>
+    public static class FileUtils
+    {
+        /// <summary>
+        /// Opens a fileStream for the desired file.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns>Thte StreamReader with the file</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="FileNotFoundException"></exception>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="IOException"></exception>
+        public static StreamReader CSVReader(string fileName) =>
+            new StreamReader(fileName);
+
+        /// <summary>
+        /// Creates a file if It doesnt exist.
+        /// </summary>
+        /// <param name="path">The path to the file</param>
+        /// <param name="fileName">The file name</param>
+        /// <returns>True iff able to create file.</returns>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="ArgumentException"></ exception >
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="PathTooLongException"></ exception >
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="IOException"></ exception >
+        /// <exception cref="NotSupportedException"></exception>
+        public static bool CreateFile(string fileName)
+        {
+            if (!File.Exists(fileName))
+            {
+                File.Create(fileName).Close();
+                return true;
+            }
+            return false;
+        }
+
+
+    }
+}
